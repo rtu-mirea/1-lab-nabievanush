cf67a4eb9a30affa75f300d9820195a80e60ccb9
Nabiev Anush <nabievanush1@gmail.com>
2019-10-16 22:13:02 +0300

    Final project

--- Main.java
+++ Main.java
@@ -1,68 +1,36 @@
 package com.company;
-import java.util.Random;
-//import java.awt.desktop.SystemEventListener;
 import java.util.Scanner;
+
 public class Main
 {
-    static void selectionSort(int num[], int size)
-    {
-        int min, temp;
-        for (int i = 0; i < size - 1; i++)
-        {
-            min = i; // запоминаем индекс текущего элемента
-            // ищем минимальный элемент чтобы поместить на место i-ого
-            for (int j = i + 1; j < size; j++)
-            {
-                if (num[j] < num[min])
-                    min = j;
-            }
-            temp = num[i];      // меняем местами i-ый и минимальный элементы
-            num[i] = num[min];
-            num[min] = temp;
-        }
-        System.out.println("----------ARRAY AFTER SORT----------");
-        for (int i = 0; i < num.length; i++)
-        {
-            System.out.print(num[i] + " ");
-        }
-    }
     public static void main(String[] args)
     {
+        int from,to,answer = -1;
+        Array arr = new Array();
+        arr.Array();
+
         Scanner in = new Scanner(System.in);
 
-        System.out.println("Enter the length of array");
-        int n = in.nextInt();
-        int array[] = new int[n];
-        for (int i = 0; i < array.length; i++)
+        while(answer != 0)
         {
-            array[i] = (int) Math.round((Math.random() * 30) - 15);
-        }
-        System.out.println("----------ARRAY----------");
-        for (int i = 0; i < array.length; i++)
-        {
-            System.out.print(array[i] + " ");
-        }
-
-        int from,to = 0;
-
-        System.out.println("Enter the range ");
-        System.out.println("FROM : ");
-        from = in.nextInt();
-        System.out.println("TO : ");
-        to = in.nextInt();
-
-        int j = 0;
-        for (int i = 0; i < array.length; i++)
-        {
-            if(array[i] >= from && array[i] <= to)
+            System.out.println("Menu");
+            System.out.println("1. Print array");
+            System.out.println("2. Sort array");
+            System.out.println("3. Print number in range...");
+            answer = in.nextInt();
+            System.out.flush();
+            if(answer == 1) arr.PrintArray();
+            else if(answer == 2) arr.selectionSort();
+            else if (answer == 3)
             {
-                System.out.print(array[i]+ " ");
-                j++;
-            }
-        }
-        System.out.println();
-        System.out.println("Answer: " + j);
 
-        selectionSort(array,array.length);
+                System.out.println("from :");
+                from = in.nextInt();
+                System.out.println("to:");
+                to = in.nextInt();
+                arr.PrintRange(from,to);
+            }
+            else System.out.println("Error");
+        }
     }
 }
